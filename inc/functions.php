45c6142073b230cc1cc1c2514f4ee1c410dbe73c
Nolt <nolt@wizzi.pl>
2021-06-01 12:49:01 +0000

    registration fixed

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,47 @@
 <?php
 
+
+ function getregdata($username, $password)
+  {
+	// generate a random salt
+	$salt = random_bytes(32);
+	
+	// calculate verifier using this salt
+	$verifier = calculateverifier($username, $password, $salt);
+	
+	// done - this is what you put in the account table!
+	return array($salt, $verifier);
+  }
+
+ function calculateverifier($username, $password, $salt)
+  {
+	// algorithm constants
+	$g = gmp_init(7);
+	$N = gmp_init('894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7', 16);
+	
+	// calculate first hash
+	$h1 = sha1(strtoupper($username . ':' . $password), TRUE);
+	
+	// calculate second hash
+	$h2 = sha1($salt.$h1, TRUE);
+	
+	// convert to integer (little-endian)
+	$h2 = gmp_import($h2, 1, GMP_LSW_FIRST);
+	
+	// g^h2 mod N
+	$verifier = gmp_powm($g, $h2, $N);
+	
+	// convert back to a byte array (little-endian)
+	$verifier = gmp_export($verifier, 1, GMP_LSW_FIRST);
+	
+	// pad to 32 bytes, remember that zeros go on the end in little-endian!
+	$verifier = str_pad($verifier, 32, chr(0), STR_PAD_RIGHT);
+	
+	// done!
+	return $verifier;
+  }
+
+
 function Register()
 {
 	if(isset($_POST['register']))
@@ -64,46 +106,6 @@ function Register()
 			$secret     = CAPTCHA_SECRET;
 			$expansion  = EXPANSION;
 
-    function getregdata($username, $password)
-    {
-        // generate a random salt
-        $salt = random_bytes(32);
-        
-        // calculate verifier using this salt
-        $verifier = calculateverifier($username, $password, $salt);
-        
-        // done - this is what you put in the account table!
-        return array($salt, $verifier);
-    }
-
- function calculateverifier($username, $password, $salt)
-    {
-        // algorithm constants
-        $g = gmp_init(7);
-        $N = gmp_init('894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7', 16);
-        
-        // calculate first hash
-        $h1 = sha1(strtoupper($username . ':' . $password), TRUE);
-        
-        // calculate second hash
-        $h2 = sha1($salt.$h1, TRUE);
-        
-        // convert to integer (little-endian)
-        $h2 = gmp_import($h2, 1, GMP_LSW_FIRST);
-        
-        // g^h2 mod N
-        $verifier = gmp_powm($g, $h2, $N);
-        
-        // convert back to a byte array (little-endian)
-        $verifier = gmp_export($verifier, 1, GMP_LSW_FIRST);
-        
-        // pad to 32 bytes, remember that zeros go on the end in little-endian!
-        $verifier = str_pad($verifier, 32, chr(0), STR_PAD_RIGHT);
-        
-        // done!
-        return $verifier;
-    }
-
 			if(ValidateUsername($username) && ValidateEmail($email))
 			{
 				$data = $con->prepare('SELECT COUNT(*) FROM account WHERE username = :username OR email = :email');
@@ -116,12 +118,16 @@ function Register()
 				{
 					if(Captcha($secret, $captcha, $ip_address))
 					{
+						$regdata = getregadata($username, $password);
+						$salt = $regdata[0];
+						$verifier = $regata[1];
+
 						$data = $con->prepare('INSERT INTO account (username, salt, verifier, email, last_ip, expansion) 
 							VALUES(:username, :salt, :verifier, :email, :ip, :expansion)');
 						$data->execute(array(
 							':username'  => $username,
-							':salt'      => ('".bindParam($salt)"'),
-							':verifier'  => ('".bin2Param($verifier)."'),
+							':salt'      => $salt,
+							':verifier'  => $verifier,
 							':email'     => $email,
 							':ip'        => $ip_address,
 							':expansion' => $expansion
